126f713165e7673db962c98d3cdca6bcbb00d31c
yocto-sothoth <yocto.sothoth@gmail.com>
2020-09-30 03:25:34 +0900

    Fix init.el

--- init.el
+++ init.el
@@ -1,36 +1,32 @@
 ;; general settings
 
-(toggle-frame-fullscreen)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(if (eq window-system nil) (menu-bar-mode -1))
-
-(setq crowd-dir "~/Dropbox/emacs"
-      os-font-height (if (eq system-type 'gnu/linux) 120 160)
+(setq backup-directory-alist '((".*" . "~/.emacs.d/backup"))
+      crowd-dir "~/Dropbox/emacs"
       custom-file "~/.emacs.d/custom.el"
-      backup-directory-alist '((".*" . "~/.emacs.d/backup"))
       delete-old-versions t
-      version-control t
-      load-prefer-newer t
+      diary-file (if (file-directory-p crowd-dir) (expand-file-name "diary" crowd-dir) "~/.emacs.d/diary")
       display-time-string-forms '((format "  %s:%s" 24-hours minutes))
-      eshell-history-size 1024
       eshell-hist-ignoredups t
-      ring-bell-function 'ignore)
+      eshell-history-size 1000
+      load-prefer-newer t
+      logo-file (if (file-directory-p crowd-dir) (expand-file-name "logo.png" crowd-dir))
+      os-font-height (if (eq system-type 'gnu/linux) 120 160)
+      ring-bell-function 'ignore
+      version-control t)
 
-(if (file-directory-p crowd-dir)
-    (setq diary-file (expand-file-name "diary" crowd-dir)
-          logo-file (expand-file-name "logo.png" crowd-dir)))
+(setq-default indent-tabs-mode nil
+              indicate-buffer-boundaries 'right)
 
-(setq-default indicate-buffer-boundaries 'right
-              indent-tabs-mode nil
-              tab-width 4)
-
-(set-face-attribute 'default nil :family "Sarasa Mono J" :height os-font-height)
-(fringe-mode '(0))
-(global-display-line-numbers-mode)
 (delete-selection-mode t)
 (display-battery-mode t)
 (display-time-mode t)
+(fringe-mode '(0))
+(global-display-line-numbers-mode)
+(scroll-bar-mode -1)
+(set-face-attribute 'default nil :family "Sarasa Mono J" :height os-font-height)
+(toggle-frame-fullscreen)
+(tool-bar-mode -1)
+(tooltip-mode -1)
 
 ;; use-package
 
@@ -39,16 +35,17 @@
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
-(if (not (package-installed-p 'use-package))
-  (package-install 'use-package))
+(unless (package-installed-p 'use-package) (package-install 'use-package))
 
 (bind-key "C-h" 'delete-backward-char)
-(bind-key "C-M-h" 'backward-kill-word)
+(bind-key "C-M-s-v" 'scroll-other-window-down)
+(setq use-package-compute-statistics t)
 
 (use-package diminish
-  :ensure t
   :config
-  (diminish 'eldoc-mode))
+  (diminish 'abbrev-mode)
+  (diminish 'eldoc-mode)
+  :ensure t)
 
 ;; each packages
 
@@ -56,75 +53,60 @@
   :ensure t)
 
 (use-package beacon
-  :ensure t
-  :init
-  (setq beacon-color "tomato"
-        beacon-size 8
-        beacon-blink-when-window-changes t)
   :config
-  (beacon-mode t))
+  (beacon-mode t)
+  :custom
+  (beacon-blink-when-window-changes t)
+  (beacon-color "olive")
+  (beacon-size 8)
+  :ensure t)
 
 (use-package company
-  :ensure t
+  :bind
+  ("C-c C-c" . company-complete)
+  :custom
+  (company-minimum-prefix-length 4)
   :diminish company-mode
-  :hook (prog-mode . company-mode)
-  :init
-  (setq company-idle-delay 0
-        company-minimum-prefix-length 4
-        company-selection-wrap-around t)
-  :config
-  (bind-keys :map company-active-map
-             ("C-p" . company-select-previous-or-abort)
-             ("C-n" . company-select-next-or-abort)))
-
-(use-package company-quickhelp
-  :ensure t
-  :hook (company-mode . company-quickhelp-mode)
-  :config
-  (company-quickhelp-mode))
+  :ensure t)
 
 (use-package counsel
-  :ensure t
-  :diminish counsel-mode
   :bind ("C-s-o" . counsel-rhythmbox)
-  :init
-  (setq counsel-yank-pop-separator "\n--------------------------------\n")
   :config
-  (counsel-mode 1))
+  (counsel-mode 1)
+  :custom
+  (counsel-yank-pop-separator "\n--------------------------------\n")
+  :diminish counsel-mode
+  :ensure t)
 
 (use-package dashboard
-  :ensure t
-  :init
-  (setq dashboard-startup-banner (cond ((eq window-system nil) 3)
-                                       ((file-exists-p logo-file) logo-file)
-                                       (t 'official))
-        dashboard-footer-icon ">"
-        dashboard-items '((agenda . 10)
-                          (recents  . 10)
-                          (bookmarks . 10)
-                          (projects . 10))
-        show-week-agenda-p t)
   :config
-  (dashboard-setup-startup-hook))
-
-(use-package doom-themes
-  :ensure t
-  :init
-  (setq doom-themes-enable-bold nil
-        doom-themes-enable-italic nil)
-  :config
-  (load-theme 'doom-sourcerer t))
+  (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-footer-icon ">")
+  (dashboard-items '((recents  . 10)
+                     (projects . 10)
+                     (bookmarks . 10)))
+  (dashboard-startup-banner (if (file-exists-p logo-file) logo-file 'official))
+  :ensure t)
 
 (use-package doom-modeline
-  :ensure t
-  :init
-  (setq doom-modeline-icon nil
-        doom-modeline-height 1
-        doom-modeline-minor-modes t)
+  :config
+  (doom-modeline-mode t)
   (set-face-attribute 'mode-line nil :family "Sarasa Mono J" :height os-font-height)
   (set-face-attribute 'mode-line-inactive nil :family "Sarasa Mono J" :height os-font-height)
+  :custom
+  (doom-modeline-height 1)
+  (doom-modeline-icon nil)
+  (doom-modeline-minor-modes t)
+  :ensure t)
+
+(use-package doom-themes
   :config
-  (doom-modeline-mode t))
+  (load-theme 'doom-sourcerer t)
+  :custom
+  (doom-themes-enable-bold nil)
+  (doom-themes-enable-italic nil)
+  :ensure t)
 
 (use-package dired
   :config
@@ -133,40 +115,39 @@
              ("r" . dired-subtree-remove)))
 
 (use-package dired-subtree
-  :ensure t
-  :commands (dired-subtree-insert dired-subtree-remove))
+  :commands (dired-subtree-insert dired-subtree-remove)
+  :ensure t)
 
 (use-package disable-mouse
-  :ensure t
-  :diminish disable-mouse-global-mode
   :config
-  (global-disable-mouse-mode))
+  (global-disable-mouse-mode)
+  :diminish disable-mouse-global-mode
+  :ensure t)
 
 (use-package exec-path-from-shell
-  :ensure t
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  :ensure t)
 
 (use-package flycheck
-  :ensure t
   :bind (("C-c n" . flycheck-next-error)
-         ("C-c p" . flycheck-previous-error)))
+         ("C-c p" . flycheck-previous-error))
+  :custom
+  (flycheck-idle-change-delay 1)
+  :ensure t)
 
 (use-package ivy
-  :ensure t
-  :diminish ivy-mode
   :config
-  (ivy-mode 1))
+  (ivy-mode 1)
+  :diminish ivy-mode
+  :ensure t)
 
 (use-package ivy-rich
-  :ensure t
   :config
-  (ivy-rich-mode 1))
+  (ivy-rich-mode 1)
+  :ensure t)
 
 (use-package japanese-holidays
-  :ensure t
-  :init
-  (setq calendar-mark-holidays-flag t)
   :config
   (setq calendar-holidays (append japanese-holidays holiday-local-holidays holiday-other-holidays))
   (defun my/japanese-holiday-show (&rest _args)
@@ -176,45 +157,46 @@
            (holiday-list (calendar-check-holidays date)))
       (when holiday-list
         (message "%s: %s" date-string (mapconcat #'identity holiday-list "; ")))))
-  (add-hook 'calendar-move-hook 'my/japanese-holiday-show))
+  (add-hook 'calendar-move-hook 'my/japanese-holiday-show)
+  :custom
+  (calendar-mark-holidays-flag t)
+  :ensure t)
+
+(use-package lsp-mode
+  :diminish lsp-mode
+  :ensure t
+  :hook (enh-ruby-mode . lsp))
 
 (use-package magit
+  :commands magit-status
   :ensure t)
 
 (use-package mozc
-  :ensure t
-  :init
-  (setq mozc-candidate-style 'echo-area
-        default-input-method "japanese-mozc-im"))
+  :custom
+  (default-input-method "japanese-mozc-im")
+  (mozc-candidate-style 'echo-area)
+  :ensure t)
 
 (use-package mozc-im
   :ensure t)
 
-(use-package lsp-mode
-  :ensure t
-  :diminish lsp-mode
-  :hook (enh-ruby-mode . lsp))
-
-(use-package prettify-greek
-  :ensure t
-  :init
-  (setq my/prettify-symbols-alist '(("Infinity" . 8734)
-                                    ("Float::INFINITY" . 8734)
-                                    ("!=" . 8800)
-                                    ("<=" . 8804)
-                                    (">=" . 8805)))
+(use-package prog-mode
   :config
   (add-hook 'enh-ruby-mode-hook (lambda () (setq prettify-symbols-alist my/prettify-symbols-alist)))
   (add-hook 'eshell-mode-hook (lambda () (setq prettify-symbols-alist my/prettify-symbols-alist)))
   (add-hook 'shell-mode-hook (lambda () (setq prettify-symbols-alist my/prettify-symbols-alist)))
-  (global-prettify-symbols-mode))
+  (global-prettify-symbols-mode)
+  :custom
+  (my/prettify-symbols-alist '(("Infinity" . 8734)
+                               ("Float::INFINITY" . 8734)
+                               ("!=" . 8800)
+                               ("<=" . 8804)
+                               (">=" . 8805))))
 
 (use-package projectile
   :ensure t)
 
 (use-package rainbow-delimiters
-  :ensure t
-  :hook (prog-mode . rainbow-delimiters-mode)
   :config
   (require 'cl-lib)
   (require 'color)
@@ -222,135 +204,144 @@
    for index from 1 to rainbow-delimiters-max-face-count
    do
    (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-     (cl-callf color-saturate-name (face-foreground face) 30))))
+     (cl-callf color-saturate-name (face-foreground face) 30)))
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package swiper
-  :ensure t
-  :bind ("C-s" . swiper))
+  :bind ("C-s" . swiper)
+  :ensure t)
 
 (use-package undo-tree
-  :ensure t
-  :diminish undo-tree-mode
   :config
-  (global-undo-tree-mode 1))
+  (global-undo-tree-mode 1)
+  :diminish undo-tree-mode
+  :ensure t)
 
 (use-package which-key
-  :ensure t
   :diminish which-key-mode
+  :ensure t
   :hook (after-init . which-key-mode))
 
 (use-package whitespace
-  :diminish whitespace-mode
-  :hook (prog-mode . whitespace-mode)
-  :init
-  (setq whitespace-style '(face trailing spaces tab-mark)
-        whitespace-action '(auto-cleanup)
-        whitespace-space-regexp "\\(\u3000+\\)")
+  :custom
+  (whitespace-action '(auto-cleanup))
+  (whitespace-space-regexp "\\(\u3000+\\)")
+  (whitespace-style '(face trailing spaces tab-mark))
   :custom-face
-  (whitespace-space ((t (:background "tomato")))))
+  (whitespace-space ((t (:background "tomato"))))
+  :diminish whitespace-mode
+  :hook ((prog-mode . whitespace-mode) (yaml-mode . whitespace-mode)))
 
 (use-package xclip
-  :if (eq system-type 'gnu/linux)
-  :ensure t
   :config
-  (xclip-mode 1))
+  (xclip-mode 1)
+  :ensure t
+  :if (eq system-type 'gnu/linux))
 
 (use-package yasnippet
-  :ensure t
-  :diminish yas-minor-mode
   :bind ("C-c y" . 'yas-describe-tables)
-  :init
-  (setq yas-new-snippet-default (concat "# -*- mode: snippet -*-\n"
-                                        "# name: ${}\n"
-                                        "# group: customize\n"
-                                        "# --\n"
-                                        "${}"))
   :config
-  (yas-global-mode 1))
+  (yas-global-mode 1)
+  :custom
+  (yas-new-snippet-default (concat "# -*- mode: snippet -*-\n"
+                                   "# name: ${}\n"
+                                   "# group: customize\n"
+                                   "# --\n"
+                                   "${}"))
+  :diminish yas-minor-mode
+  :ensure t)
 
 (use-package yasnippet-snippets
-  :ensure t
-  :init
-  (setq reject-snippets-list '("ruby-mode/pry" "ruby-mode/when"))
   :config
   (require 'cl-lib)
   (defun reject-snippets ()
-    "Reject some yasnippet-snippets snnipets."
+    "Reject some yasnippet-snippets snippets."
     (interactive)
     (cl-loop for e in reject-snippets-list
              do (delete-file (expand-file-name e yasnippet-snippets-dir)))
-    (yas-reload-all)))
-
-;; ruby
-
-(use-package enh-ruby-mode
-  :ensure t
-  :mode (("\\.rb\\'" . enh-ruby-mode))
-  :custom-face
-  (erm-syn-warnline ((t (:underline nil))))
-  (erm-syn-errline ((t (:underline nil)))))
-
-(use-package ruby-electric
-  :ensure t
-  :diminish ruby-electric-mode
-  :hook (enh-ruby-mode . ruby-electric-mode))
-
-(use-package inf-ruby
-  :ensure t
-  :hook (enh-ruby-mode . inf-ruby-minor-mode)
-  :init
-  (setq inf-ruby-default-implementation "pry"))
+    (yas-reload-all))
+  :custom
+  (reject-snippets-list '("prog-mode/todo"
+                          "ruby-mode/pry"
+                          "ruby-mode/when"))
+  :ensure t)
 
 ;; crystal
 
 (use-package crystal-mode
-  :ensure t
-  :mode (("\\.cr\\'" . crystal-mode)))
+  :mode (("\\.cr\\'" . crystal-mode))
+  :ensure t)
+
+(use-package flycheck-crystal
+  :config
+  (add-hook 'crystal-mode-hook 'flycheck-mode)
+  :ensure t)
 
 ;; lisp
 
 (use-package slime
-  :ensure t
   :commands slime
-  :init
-  (setq inferior-lisp-program "clisp"))
+  :custom
+  (inferior-lisp-program "clisp")
+  :ensure t)
 
 (use-package slime-company
   :ensure t)
 
-;; web develop
+;; ruby
 
-(use-package web-mode
+(use-package enh-ruby-mode
+  :custom-face
+  (erm-syn-errline ((t (:underline nil))))
+  (erm-syn-warnline ((t (:underline nil))))
   :ensure t
-  :mode (("\\.html?\\'" . web-mode)
-         ("\\.s?css\\'" . web-mode)
-         ("\\.erb\\'" . web-mode)
-         ("\\.php\\'" . web-mode)
-         ("\\.jsx?\\'" . web-mode))
-  :init
-  (setq web-mode-code-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-enable-auto-closing t
-        web-mode-enable-auto-pairing t
-        web-mode-markup-indent-offset 2
-        web-mode-script-padding 2
-        web-mode-style-padding 2))
+  :mode (("\\.rb\\'" . enh-ruby-mode)))
+
+(use-package inf-ruby
+  :custom
+  (inf-ruby-default-implementation "pry")
+  :ensure t
+  :hook (enh-ruby-mode . inf-ruby-minor-mode))
+
+(use-package ruby-electric
+  :diminish ruby-electric-mode
+  :ensure t
+  :hook (enh-ruby-mode . ruby-electric-mode))
+
+;; web develop
 
 (use-package slim-mode
   :ensure t
   :mode (("\\.slim\\'" . slim-mode)))
 
-(use-package yaml-mode
+(use-package web-mode
+  :custom
+  (web-mode-code-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-enable-auto-closing t)
+  (web-mode-enable-auto-pairing t)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-script-padding 2)
+  (web-mode-style-padding 2)
   :ensure t
-  :mode (("\\.yml\\'" . yaml-mode)))
+  :mode (("\\.erb\\'" . web-mode)
+         ("\\.html?\\'" . web-mode)
+         ("\\.jsx?\\'" . web-mode)
+         ("\\.php\\'" . web-mode)
+         ("\\.s?css\\'" . web-mode)))
 
 ;; document
 
 (use-package markdown-mode
+  :custom
+  (markdown-command "pandoc")
   :ensure t
-  :mode (("\\.md\\'" . gfm-mode))
-  :init
-  (setq markdown-command "pandoc"))
+  :mode (("\\.md\\'" . gfm-mode)))
+
+(use-package yaml-mode
+  :ensure t
+  :mode (("\\.yml\\'" . yaml-mode)))
 
 (use-package yatex
   :ensure t
@@ -362,18 +353,18 @@
   :bind (("C-c a" . 'org-agenda)
          ("C-c c" . 'org-capture)
          ("C-c l" . 'org-store-link))
-  :init
-  (setq org-directory (if (file-directory-p crowd-dir) crowd-dir "~/org")
-        org-default-notes-file "inbox.org"
-        org-agenda-files '("~/Dropbox/emacs/inbox.org")
-        org-agenda-current-time-string "now"
-        org-agenda-time-grid '((weekly today required-timed) (1000 1900) "-" "-")
-        org-agenda-time-leading-zero t
-        org-log-done 'time
-        org-capture-templates
-        '(("i" "Inbox" entry (file+headline "inbox.org" "Inbox") "* INBOX %? %U")
-          ("w" "Wishlist" entry (file+headline "inbox.org" "Wishlist") "* INBOX %? %U")
-          ("n" "Note" entry (file+headline "note.org" "Note") "* %? %U")
-          ("e" "English" entry (file+headline "word.org" "Word") "* %?"))
-        org-todo-keywords
-        '((sequence "INBOX(i)" "TODO(t)" "SOMEDAY(s)" "HIGH(h)" "LOW(l)" "|" "CANCELLED(c)" "DONE(d)"))))
+  :custom
+  (org-agenda-current-time-string "now")
+  (org-agenda-files '("~/Dropbox/emacs/inbox.org"))
+  (org-agenda-time-grid '((weekly today required-timed) (1000 1900) "-" "-"))
+  (org-agenda-time-leading-zero t)
+  (org-capture-templates
+   '(("i" "Inbox" entry (file+headline "inbox.org" "Inbox") "* INBOX %?")
+     ("w" "Wishlist" entry (file+headline "inbox.org" "Wishlist") "* INBOX %?")
+     ("n" "Note" entry (file+headline "note.org" "Note") "* %?")
+     ("e" "English" entry (file+headline "word.org" "Word") "* %?")))
+  (org-default-notes-file "inbox.org")
+  (org-directory (if (file-directory-p crowd-dir) crowd-dir "~/org"))
+  (org-log-done 'time)
+  (org-todo-keywords
+   '((sequence "INBOX(i)" "TODO(t)" "SOMEDAY(s)" "HIGH(h)" "LOW(l)" "|" "CANCELLED(c)" "DONE(d)"))))
